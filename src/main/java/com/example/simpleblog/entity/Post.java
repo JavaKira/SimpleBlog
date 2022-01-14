7847c3d0b07e37622dbb6fe98e056e432f063026
JavaKira <JavaKira@yandex.ru>
2022-01-14 17:01:19 +0300

    Added Post text and date column

--- src/main/java/com/example/simpleblog/entity/Post.java
+++ src/main/java/com/example/simpleblog/entity/Post.java
@@ -1,20 +1,49 @@
 package com.example.simpleblog.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String text;
+    private Date date;
+
+    public Post(Long id, String text, Date date) {
+        this.id = id;
+        this.text = text;
+        this.date = date;
+    }
+
+    public Post() {
+        date = new Date();
+    }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    @Id
     public Long getId() {
         return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }

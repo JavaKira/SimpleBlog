84974132fa97b2027723e82ec9130f33fee2b957
JavaKira <JavaKira@yandex.ru>
2022-01-15 17:32:04 +0300

    Added Post text change

--- src/main/java/com/example/simpleblog/controller/BlogController.java
+++ src/main/java/com/example/simpleblog/controller/BlogController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,9 +25,37 @@ public class BlogController {
     {
         List<Post> posts = postService.getAll();
         Collections.reverse(posts);
+        if (request.getParameter("editable_post_id") != null) {
+            try {
+                model.addAttribute("editable_post",
+                        (Post) postService.getByID(Integer.parseInt(request.getParameter("editable_post_id"))));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+
         model.addAttribute("posts", posts);
         model.addAttribute("authorized", request.getSession().getAttribute("authorized"));
         return "blog/news";
+    }
+
+    @PatchMapping
+    public String rewritePost(HttpServletRequest request)
+    {
+        System.out.println(request.getParameter("editable_post_id"));
+        if (request.getParameter("editable_post_id") != null)
+        {
+            try {
+                Post editablePost = postService.getByID(Integer.parseInt(request.getParameter("editable_post_id")));
+                editablePost.setText(request.getParameter("text"));
+                postService.add(editablePost);
+            } catch (NumberFormatException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return "redirect:/blog";
     }
 
     @PostMapping
